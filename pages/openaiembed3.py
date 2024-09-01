ba57dee3a194611dce65f173cd09c398a52ccaf0
Karl Kablisk <karlkablisk@gmail.com>
2024-09-01 22:40:14 +0900

    Update openaiembed3.py

--- pages/openaiembed3.py
+++ pages/openaiembed3.py
@@ -31,7 +31,7 @@ class OpenAIStreamlitApp:
         top_index = np.argmax(similarities)
         return contexts[top_index]
 
-    def count_tokens(self, text, model="gpt-4o-mini"):
+    def count_tokens(self, text, model="gpt-4o"):
         """Count the number of tokens in the given text."""
         enc = tiktoken.encoding_for_model(model)
         return len(enc.encode(text))
